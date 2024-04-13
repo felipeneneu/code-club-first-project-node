c6753ea984c094a34fa002620d55d5e926e97fc0
felipeneneu <feliperpg3@gmail.com>
2024-04-13 10:15:27 -0300

    try e catch

--- index.js
+++ index.js
@@ -38,14 +38,19 @@ app.get('/users', (request, response) => {
 })
 
 app.post('/users', (request, response) => {
+    try {
     const { name, age } = request.body
 
+        if (age < 18) throw new Error("Only allowed users over 18 years old")
 
     const user = { id: uuid.v4(), name, age }
 
-    users.push(user)
+        users.push(user)
 
     return response.status(201).json(user)
+    } catch (err) {
+        return response.status(500).json({ error: err.message })
+    }
 })
 
 app.put('/users/:id', checkUserId, (request, response) => {
